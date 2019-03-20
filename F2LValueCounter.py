f7e99ea83c7a234a61bf606de0fb0532bab23afe
phillyb7 <philip.brownlow777@gmail.com>
2019-03-20 20:14:08 +0000

    Created the cube file to be used by the RPi and deleted the functions not used by the Pi

---
+++ F2LValueCounter.py
@@ -0,0 +1,18 @@
+def counter():
+    numbers = [["36", "13", "24"], ["38", "22", "33"], ["40", "31", "47"], ["42", "45", "15"]]
+
+    for i in range(len(numbers)):
+        f = open("F2LValues.txt", "r")
+        total = 0
+        for line in f:
+            line = line.split("\n")[0].split("\r")[0].split(", ")
+            if line[5] == numbers[i][0] and line[6] == numbers[i][1] and line[7] == numbers[i][2]:
+                total += 1
+        print(total)
+        f.close()
+
+def main():
+    counter()
+
+if __name__ == '__main__':
+    main()
